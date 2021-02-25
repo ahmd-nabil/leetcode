f7ade9a69bb28ad595f99e1347c909c3d29ee0ce
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-02-25 19:37:06 +0200

    Add some explanation for BrokenCalculator problem

--- February Challenge 2021/src/BrokenCalculator/BackwardsSolution.java
+++ February Challenge 2021/src/BrokenCalculator/BackwardsSolution.java
@@ -1,6 +1,16 @@
 package BrokenCalculator;
 
 public class BackwardsSolution {
+    /**
+     * I would like to add some explanations for working backwards:
+     * If with subtraction and multiplication, the effect of earlier subtraction will be amplified by later
+     * multiplications,hence, greedily doing multiplication might not reach optimal solution as an additional
+     * early subtraction can have a huge effect.
+     * Whereas with addition and division, earlier addition will be diminished by later divisions.
+     * It is thus always better to do division wherever possible.
+     *
+     * By Alex Cai
+    * */
     public int brokenCalc(int x, int y) {
         int ans = 0;
         while(y > x) {
