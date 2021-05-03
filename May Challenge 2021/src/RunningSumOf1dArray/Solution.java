9662385a12f7f5008cd47616343caf162eb7320d
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-05-03 21:09:57 +0200

    Add 3 May Challenge "Running Sum of 1d Array"

---
+++ May Challenge 2021/src/RunningSumOf1dArray/Solution.java
@@ -0,0 +1,10 @@
+package RunningSumOf1dArray;
+
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i=1; i<nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
