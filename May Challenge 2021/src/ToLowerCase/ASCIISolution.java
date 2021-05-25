e8fa173e91f4015833b748ab40308ccc9a7067ac
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-05-25 16:37:45 +0200

    Add 24 May Challenge "To Lower Case"

---
+++ May Challenge 2021/src/ToLowerCase/ASCIISolution.java
@@ -0,0 +1,12 @@
+package ToLowerCase;
+
+public class ASCIISolution {
+    public String toLowerCase(String s) {
+        char[] chars = s.toCharArray();
+        for(int i=0; i<chars.length; i++) {
+            if(chars[i] >= 'A' && chars[i] <= 'Z')
+                chars[i] += 32;
+        }
+        return new String(chars);
+    }
+}
